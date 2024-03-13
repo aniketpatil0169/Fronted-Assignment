32b9dc5a3a9bddba6e9787e0026211dffbaef4dc
aniketpatil0169 <148536939+aniketpatil0169@users.noreply.github.com>
2024-03-13 15:42:53 +0530

    Create Redirection.js

---
+++ Redirection.js
@@ -0,0 +1,14 @@
+function redirectPage() {
+    window.open('http://www.google.com');
+}
+
+function redirectAnotherPage() {
+   // window.open('getsetcontent.html');
+   // window.open('getsetcontent.html', '_self');
+    //window.open('getsetcontent.html', 'mywindow');
+    window.open('getsetcontent.html','mywindow');
+}
+
+function closePageWindow() {
+    window.close('mywindow');
+}
