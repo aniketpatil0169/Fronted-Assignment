e29e118bfd4ef1c577b4b94c2dff47b648a880bf
aniketpatil0169 <148536939+aniketpatil0169@users.noreply.github.com>
2024-03-16 22:51:13 +0530

    Create StringFunction.js

---
+++ StringFunction.js
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title></title>
+</head>
+<body>
+    <h2>String Function</h2>
+
+    Input:
+    <textarea rows="5" cols="100" id="input"></textarea>
+    <br /><br />
+    <input type="button" value="Click Me!!!" onclick="btnClick()"  /> |
+    <input type="button" value="Clear" onclick="clearValues()" />
+    <br /><br />
+    Output: <textarea rows="5" cols="100" id="output"></textarea>
+
+    <script src="Script/StringFunction.js"></script>
+</body>
+</html>
